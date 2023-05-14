e7d791711651e7a6697805ef70e1c06462bebe0f
Jiří Bednář <jiri.bednar@isocta.com>
2023-05-15 09:46:42 +1000

    Reintroduced loop delay for sunset/sunrise

--- tasks/rainbow.py
+++ tasks/rainbow.py
@@ -1,13 +1,17 @@
 from .common import *
 
 def rainbow(strip: ws.PixelStrip, exit_event: threading.Event, arg = None):
-    n = 1
+    t_rate = 0.03
+    i_rate = 0.003
+    start_time = time.time()
+    elapsed_time = 0
     while not exit_event.is_set():
-        n+=1
-        
         for i in range(strip.numPixels()):
-            color = hsv_to_rgb(n*0.003+i*0.003,1,1)
+            # hue progresses along the strip (i) and with time (elapsed_time)
+            # saturation and value are always 1 (full)
+            color = hsv_to_rgb(elapsed_time*t_rate+i*i_rate,1,1)
             color_obj = ws.Color(*color)
             strip.setPixelColor(i,color_obj)
         strip.show()
         exit_event.wait(0.1)
+        elapsed_time = time.time() - start_time
--- tasks/sunset.py
+++ tasks/sunset.py
@@ -41,8 +41,8 @@ def sunset(strip: ws.PixelStrip, exit_event: threading.Event, arg = None):
         # When all the pixels are set, send the data to the LEDs
         strip.show()
         # Wait a bit before looping again
-        #exit_event.wait(0.1)
-        # ...or not, because the loop takes a while anyway
+        exit_event.wait(0.1)
+        # The loop takes time anyway, but we don't want to starve the HTTP server
         # Update the elapsed time and loop again
         elapsed_time = time.time() - start_time
     # At the end of the sunset sequence, turn off the LEDs
