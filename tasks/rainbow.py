caf76dc3db4b574ea997f3dce1e21741ac68b22c
Jiří Programagor <jiri.bednar@isocta.com>
2023-05-21 23:49:02 +1000

    Improved rainbow for looped strip

--- tasks/rainbow.py
+++ tasks/rainbow.py
@@ -1,15 +1,20 @@
 from .common import *
 
 def rainbow(strip: ws.PixelStrip, exit_event: threading.Event, arg = None):
-    t_rate = 0.03
+    t_rate = 0.04
     i_rate = 0.003
     start_time = time.time()
     elapsed_time = 0
     while not exit_event.is_set():
         for i in range(strip.numPixels()):
+            # Strip has U topology, so loop back in the middle
+            if i < range(strip.numPixels()):
+                pos = i
+            else:
+                pos = strip.numPixels() - i
             # hue progresses along the strip (i) and with time (elapsed_time)
             # saturation and value are always 1 (full)
-            color = hsv_to_rgb(elapsed_time*t_rate+i*i_rate,1,1)
+            color = hsv_to_rgb(elapsed_time*t_rate+pos*i_rate,1,1)
             color_obj = ws.Color(*color)
             strip.setPixelColor(i,color_obj)
         strip.show()
